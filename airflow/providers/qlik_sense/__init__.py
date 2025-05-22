d681b9c72f60e73ddeb76dbfa3d57fa7dd54c4c7
Clément PARSY <clementparsy02@gmail.com>
2025-05-22 19:03:38 +0200

    upgrade to airflow 3.0

--- airflow/providers/qlik_sense/__init__.py
+++ airflow/providers/qlik_sense/__init__.py
@@ -1,4 +1,7 @@
-## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features. We recognize it's a bit unclean to define these in multiple places, but at this point it's the only workaround if you'd like your custom conn type to show up in the Airflow UI.
+from __future__ import annotations
+import packaging.version
+from airflow import __version__ as airflow_version
+
 def get_provider_info():
     return {
         "package-name": "airflow-provider-qlik-sense-client-managed",
